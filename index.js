26c0d17acb16d4477fb433d553bdfc7cf56b5963
Leah Salzano <leahsalzano@Leahs-MacBook-Air.local>
2020-05-11 20:29:35 -0400

    made a spicy website

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'html5up-identity')))
+  .use(express.static(path.join(__dirname, 'html5up-dimension')))
   .get('/*', function(_, res) { res.redirect('/') })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
